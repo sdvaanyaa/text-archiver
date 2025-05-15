1176564bed331593dfa55cb59a02fbe4c19790ed
Ivan A <sd.vaanyaa@yandex.ru>
2025-05-15 22:45:00 +0300

    add project skeleton

---
+++ cmd/vlc.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"errors"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var vlcCmd = &cobra.Command{
+	Use:   "vlc",
+	Short: "Pack file using variable-length code",
+	Run:   pack,
+}
+
+const packedExtension = "vlc"
+
+var ErrEmptyPath = errors.New("path to file is not specified")
+
+func pack(_ *cobra.Command, args []string) {
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+
+	filePath := args[0]
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+
+	// data -> Encode(data) -> packed
+	// packed := Encode(data)
+	packed := "" + string(data) // TODO
+
+	err = os.WriteFile(packedFileName(filePath), []byte(packed), 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+// /path/to/file/file.txt -> file.vlc
+func packedFileName(path string) string {
+	fileName := filepath.Base(path)
+	ext := filepath.Ext(fileName)
+	baseName := strings.TrimSuffix(fileName, ext)
+
+	return baseName + "." + packedExtension
+}
+
+func init() {
+	packCmd.AddCommand(vlcCmd)
+}
